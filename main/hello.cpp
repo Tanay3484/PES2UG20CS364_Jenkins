7ffa127564b2def22e2924f811cfa9805d63a40f
tanay3484 <tanay8705@gmail.com>
2023-02-16 20:59:48 +0530

    Modified Hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,7 @@ int main(void) {
   cout << "This code is written by SRN : PES2UG20CS364 ; Name : Tanay Singh"<<endl;
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
+  cout << "This is a new line"<<endl;
   cout << "I have successfully built and run\n";
   return 0;
 }
